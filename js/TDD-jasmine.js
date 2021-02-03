6eb0dd1e6573eb517eef86ba1a5364cd312066cc
samx23 <skalexsong@gmail.com>
2021-02-03 20:32:50 +0700

    add more test and implementation

--- js/TDD-jasmine.js
+++ js/TDD-jasmine.js
@@ -1,15 +1,17 @@
 // Test Driven Development test using Jasmine and a detect triangle function.
 // 1. 3 panjang segitiga (sisi a, sisi b, sisi c) telah diterima.
-//    -Bila salah satu sisi bernilai < 1, maka gagal.
-//    -Bila salah satu sisi tidak bernilaikan integer, maka gagal.
+//    - Bila salah satu sisi bernilai < 1, maka gagal.
+//    - Bila salah satu sisi tidak bernilaikan integer, maka gagal.
+//    - Bila sisi a + b bernilai <= c, maka gagal
 // 2. Mendeteksi segitiga
-//    -Kembalikan “Segitiga Sama Sisi” bila a, b, dan c sama.
-//    -Kembalikan “Segitiga Sama Kaki” bila:
-//    -Sisi a, b sama, namun c berbeda.
-//    -Sisi a, c sama, namun b berbeda.
-//    -Sisi b, c sama, namun a berbeda.
-//    -Kembalikan “Segitiga Sembarang” bila a, b dan c berbeda panjang.
-//    -Pendeteksian segitiga selesai.
+//    - Kembalikan “Segitiga Sama Sisi” bila a, b, dan c sama.
+//    - Kembalikan “Segitiga Sama Kaki” bila:
+//    - Sisi a, b sama, namun c berbeda.
+//    - Sisi a, c sama, namun b berbeda.
+//    - Sisi b, c sama, namun a berbeda.
+//    - Kembalikan “Segitiga Sembarang” bila a, b dan c berbeda panjang.
+//    - Pendeteksian segitiga selesai.
+// 3. Pendeteksian segitiga selesai
 const detectTriangle = (sideA, sideB, sideC) => {
   [sideA, sideB, sideC] = [sideA, sideB, sideC]
     .map((side) => {
@@ -24,6 +26,10 @@ const detectTriangle = (sideA, sideB, sideC) => {
       return side;
     })
     .sort();
+
+  if (sideA + sideB <= sideC) {
+    throw new Error("Triangle does not conform inequality principle");
+  }
 
   if (sideA === sideB && sideA === sideC) {
     return "Equilateral Triangle";
@@ -58,19 +64,28 @@ describe("Detect the triangle", () => {
     );
   });
 
+  it("Should detech inequality problem", () => {
+    expect(() => detectTriangle(4, 1, 2)).toThrowError(
+      "Triangle does not conform inequality principle"
+    );
+    expect(() => detectTriangle(5, 1, 3)).toThrowError(
+      "Triangle does not conform inequality principle"
+    );
+  });
+
   it("Should detect Equilateral Triangle if all sides are equal", () => {
     expect(detectTriangle(1, 1, 1)).toEqual("Equilateral Triangle");
     expect(detectTriangle(1, 2, 2)).not.toEqual("Equilateral Triangle");
   });
 
   it("Should detect Isosceles Triangle if only two sides are equal", () => {
-    expect(detectTriangle(1, 1, 2)).toEqual("Isosceles Triangle");
-    expect(detectTriangle(1, 2, 1)).toEqual("Isosceles Triangle");
+    expect(detectTriangle(2, 2, 3)).toEqual("Isosceles Triangle");
+    expect(detectTriangle(4, 2, 4)).toEqual("Isosceles Triangle");
     expect(detectTriangle(1, 2, 2)).toEqual("Isosceles Triangle");
     expect(detectTriangle(4, 1, 4)).toEqual("Isosceles Triangle");
   });
 
   it("should detect Scalene Triangle if no sides are the same", () => {
-    expect(detectTriangle(1, 2, 3)).toEqual("Scalene Triangle");
+    expect(detectTriangle(2, 4, 3)).toEqual("Scalene Triangle");
   });
 });
